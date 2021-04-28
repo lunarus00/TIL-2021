f6027d68f802881661566742010545111d888bf3
lunarus00 <moongur1900@naver.com>
2021-04-28 13:06:15 +0900

    210428 | 변수와 데이터 타입의 변경

---
+++ 04/Data_type_operation/Casting.java
@@ -0,0 +1,20 @@
+
+public class Casting {
+
+	public static void main(String[] args) {
+		
+		double a = 1.1;
+		double b = 1;
+		
+		System.out.println(b);
+		
+		int c = (int) 1.1;
+		System.out.println(c);
+		
+		String d = Integer.toString(1);
+		System.out.println(d);
+		System.out.println(d.getClass());
+
+	}
+
+}
---
+++ 04/Data_type_operation/Variable.java
@@ -0,0 +1,17 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		int a = 1; // Number -> Integer (정수)
+		System.out.println(a);
+		
+		double b = 1.1; // Real Number -> Double (실수)
+		System.out.println(b);
+		
+		String c = "Hello World"; // 문자열
+		System.out.println(c);
+
+	}
+
+}
