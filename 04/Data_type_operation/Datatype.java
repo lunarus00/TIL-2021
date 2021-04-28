7ce3ddcce3e8bc5fda9eac07adaa6d354f6db3f5
lunarus00 <moongur1900@naver.com>
2021-04-28 12:36:34 +0900

    210428 | 데이터 타입과 문자열

---
+++ 04/Data_type_operation/Datatype.java
@@ -0,0 +1,15 @@
+public class Datatype {
+	public static void main(String[] args) {
+		System.out.println(6); // Number
+		System.out.println("6"); // String
+		
+		System.out.println(6+6); // 12
+		System.out.println("6" + "6"); // 66 문자열의 덧셈은 두 문자를 이어붙임
+		
+		System.out.println(6*6); // 36
+		// System.out.println("6" * "6"); 문자열은 곱셈이 불가능
+		
+		System.out.println("1111".length()); // 문자열의 길이를 구함
+		// System.out.println(1111.length()); 숫자의 길이를 구하는 연산은 없음
+	}
+}
---
+++ 04/Data_type_operation/StringApp.java
@@ -0,0 +1,19 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		 
+		System.out.println("Hello World"); // String 문자열
+		System.out.println('H'); // Character 문자
+		System.out.println("H"); 
+		
+		System.out.println("Hello "
+				+ "World");
+		// \n 은 new line의 약자
+		System.out.println("Hello \nWorld");
+		
+		// \" 시 "가 일반 문자로 인식
+		System.out.println("Hello \"World\""); // Hello "World"
+	}
+
+}
